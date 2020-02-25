2f1cb84890cc82cc1188efc7b1088be8d79b4414
chernushov88 <chernushov88@mail.ru>
2020-02-25 18:41:53 +0200

    SPA blood

---
+++ blood/js/vue/component.js
@@ -0,0 +1,26 @@
+Vue.component('home', {
+	template: `
+
+	`,
+	data: function () {
+		return { 
+			mousein: false,
+			check_mobile: mobile,
+			initY : 0,
+			finishY : 0,
+		}
+	},
+	computed: {
+
+	},
+	props:{
+		
+		
+	},
+	
+	methods: {
+
+	}
+})
+
+ 
---
+++ blood/js/vue/vue.js
@@ -0,0 +1,20 @@
+var allHome = new Vue({
+	el: '#home-container',
+	data:{
+		type : '',
+
+		
+	},	
+	mounted() {
+
+	},
+	methods: {
+
+	
+
+		
+	},
+	
+	
+})
+ 
